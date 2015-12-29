df1b8bf05377e0e22fbfe657c498d1acecddc5a9
Justin Varghese John <justinjohn107@gmail.com>
2015-12-28 20:44:17 -0500

    Update removing bounds and types option

--- app/scripts/create-event.js
+++ app/scripts/create-event.js
@@ -267,15 +267,15 @@ var APP = (function() {
         }
     };
 
-    var defaultBounds = new google.maps.LatLngBounds(
-      new google.maps.LatLng(-33.8902, 151.1759),
-      new google.maps.LatLng(-33.8474, 151.2631));
+    // var defaultBounds = new google.maps.LatLngBounds(
+    //   new google.maps.LatLng(-33.8902, 151.1759),
+    //   new google.maps.LatLng(-33.8474, 151.2631));
 
     var input = document.getElementById('location');
-    var options = {
-      bounds: defaultBounds,
-      types: ['establishment']
-    };
+    // var options = {
+    //   bounds: defaultBounds,
+    //   types: ['establishment']
+    // };
 
-    autocomplete = new google.maps.places.Autocomplete(input, options);
+    autocomplete = new google.maps.places.Autocomplete(input);
 })();

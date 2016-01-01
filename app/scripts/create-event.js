66ada5491aa261e5f9b27511276293c91b9ddd6e
Justin Varghese John <justinjohn107@gmail.com>
2015-12-31 20:01:56 -0600

    Refactor to fix JSLint issues

--- app/scripts/create-event.js
+++ app/scripts/create-event.js
@@ -1,245 +1,245 @@
-var APP = (function() {
-    /**
-     * Creates an Array of DOM nodes that match the selector
-     * @param selector {string} CSS selector - selector to match against
-     * @return {array} Array of DOM nodes
-     */
-    function getDomNodeArray(selector) {
-        var nodes = Array.prototype.slice.apply(document.querySelectorAll(selector));
-        if (!nodes) {
-            nodes = [];
-        }
-        return nodes;
+(function() {
+  /**
+   * Creates an Array of DOM nodes that match the selector
+   * @param selector {string} CSS selector - selector to match against
+   * @return {array} Array of DOM nodes
+   */
+  function getDomNodeArray(selector) {
+    var nodes = Array.prototype.slice.apply(document.querySelectorAll(selector));
+    if (!nodes) {
+      nodes = [];
+    }
+    return nodes;
+  }
+
+  /*
+  Adding guests
+   */
+  var newGuest = document.querySelector('input.new-guest');
+  var people = document.querySelector('.attendees');
+  var guestsDisplay = document.querySelector('.card-detail-actual.guests');
+  newGuest.onkeydown = function(evt) {
+    if (evt.keyIdentifier === "Enter") {
+      let enteredPerson = document.createElement('div');
+      let enteredPerson2 = document.createElement('div');
+      let deletePerson = document.createElement('button');
+      deletePerson.innerHTML = "-";
+      deletePerson.parent = enteredPerson;
+
+      deletePerson.onclick = function() {
+        let dataAttr = this.parent.getAttribute('data-guest');
+        let guestToRemove = document.querySelector(`[data-guest="${dataAttr}"]`);
+        people.removeChild(this.parent);
+        guestsDisplay.removeChild(guestToRemove); // remove from card-detail preview
+      };
+      enteredPerson.innerHTML = newGuest.value;
+      enteredPerson.setAttribute('data-guest', newGuest.value); // for easy access later
+      enteredPerson.appendChild(deletePerson);
+      // add to card-detail preview
+      enteredPerson2.innerHTML = newGuest.value;
+      enteredPerson2.setAttribute('data-guest', newGuest.value); // for easy access later
+      guestsDisplay.appendChild(enteredPerson2);
+
+      people.appendChild(enteredPerson);
+      newGuest.value = "";
+    }
+  };
+  /*
+  Adding functionality to hide/show guest in card-detail preview
+   */
+  var attendeesCheckbox = document.getElementById('attendees-list');
+  var guestsDisplayContainer = document.querySelector('.card-detail.guests');
+  attendeesCheckbox.onclick = function() {
+    if (this.checked === true) {
+      guestsDisplayContainer.classList.remove('hidden')
+    } else {
+      guestsDisplayContainer.classList.add('hidden');
+    }
+  }
+
+  /*
+  class="model model-*" Refer to inputs by the name after class model-*
+   */
+  function Model() {
+    var self = this;
+
+    function Binding(elem, value) {
+      elem = elem || null;
+      value = value || null;
+
+      this.elem = elem;
+      this.value = value;
+      this.hasChanged = false;
+      this.oninput = function() {};
     }
 
-    /*
-    Adding guests
-     */
-    var newGuest = document.querySelector('input.new-guest');
-    var people = document.querySelector('.attendees');
-    var guestsDisplay = document.querySelector('.card-detail-actual.guests');
-    newGuest.onkeydown = function(evt) {
-        if (evt.keyIdentifier === "Enter") {
-            let enteredPerson = document.createElement('div');
-            let enteredPerson2 = document.createElement('div');
-            let deletePerson = document.createElement('button');
-            deletePerson.innerHTML = "-";
-            deletePerson.parent = enteredPerson;
+    // pulls values from elems of class="model model-*" to create Model's raw info and set up 1-way binding
+    getDomNodeArray('.model').forEach(function(elem) {
+      elem.classList.forEach(function(className) {
+          var possiblyMatch = className.match(/model\-/g);
+          if (possiblyMatch) {
+              // create the Model value
+              var name = className.slice(6);
+              var value = elem.value;
 
-            deletePerson.onclick = function() {
-                let dataAttr = this.parent.getAttribute('data-guest');
-                let guestToRemove = document.querySelector(`[data-guest="${dataAttr}"]`);
-                people.removeChild(this.parent);
-                guestsDisplay.removeChild(guestToRemove); // remove from card-detail preview
-            };
-            enteredPerson.innerHTML = newGuest.value;
-            enteredPerson.setAttribute('data-guest', newGuest.value); // for easy access later
-            enteredPerson.appendChild(deletePerson);
-            // add to card-detail preview
-            enteredPerson2.innerHTML = newGuest.value;
-            enteredPerson2.setAttribute('data-guest', newGuest.value); // for easy access later
-            guestsDisplay.appendChild(enteredPerson2);
+              // set default values
+              if (name.indexOf("Month") > -1) {
+                  value = value - 1;
+              } else if (name.indexOf("Time" > -1)) {
+                  value = value || '00:00';
+              }
 
-            people.appendChild(enteredPerson);
-            newGuest.value = "";
-        }
-    };
-    /*
-    Adding functionality to hide/show guest in card-detail preview
-     */
-    var attendeesCheckbox = document.getElementById('attendees-list');
-    var guestsDisplayContainer = document.querySelector('.card-detail.guests');
-    attendeesCheckbox.onclick = function() {
-      if (this.checked === true) {
-        guestsDisplayContainer.classList.remove('hidden')
-      } else {
-        guestsDisplayContainer.classList.add('hidden');
-      }
-    }
+              self[name] = self[name] || new Binding(elem, value);
+              elem.binding = elem.binding || self[name];
 
-    /*
-    class="model model-*" Refer to inputs by the name after class model-*
-     */
-    function Model() {
-        var self = this;
+              // bind data oninput
+              elem.oninput = function() {
+                  self[name].hasChanged = true;
+                  self[name] = self[name] || new Binding(elem, value);
+                  self[name].value = elem.value;
 
-        function Binding(elem, value) {
-            elem = elem || null;
-            value = value || null;
-
-            this.elem = elem;
-            this.value = value;
-            this.hasChanged = false;
-            this.oninput = function() {};
-        }
-
-        // pulls values from elems of class="model model-*" to create Model's raw info and set up 1-way binding
-        getDomNodeArray('.model').forEach(function(elem) {
-            elem.classList.forEach(function(className) {
-                var possiblyMatch = className.match(/model\-/g);
-                if (possiblyMatch) {
-                    // create the Model value
-                    var name = className.slice(6);
-                    var value = elem.value;
-
-                    // set default values
-                    if (name.indexOf("Month") > -1) {
-                        value = value - 1;
-                    } else if (name.indexOf("Time" > -1)) {
-                        value = value || '00:00';
-                    }
-
-                    self[name] = self[name] || new Binding(elem, value);
-                    elem.binding = elem.binding || self[name];
-
-                    // bind data oninput
-                    elem.oninput = function() {
-                        self[name].hasChanged = true;
-                        self[name] = self[name] || new Binding(elem, value);
-                        self[name].value = elem.value;
-
-                        // for callbacks
-                        self[name].oninput();
-                    };
-                }
-            });
-        });
-    };
-
-    /*
-    To create a placeholder effect. Assumes that display element starts with 'placeholder' class
-     */
-    function displayWithPlaceholder(inputBinding, displayElem, placeholder) {
-        if (displayElem.classList.contains('placeholder')) {
-            displayElem.classList.remove('placeholder');
-        };
-        if (inputBinding.value === "") {
-            inputBinding.value = placeholder;
-            // let the model know that input has gone back to default
-            inputBinding.hasChanged = false;
-
-            displayElem.classList.add('placeholder');
-        }
-        displayElem.innerHTML = inputBinding.value;
-    };
-    var model = new Model();
-
-    // Update the view oninput
-    model.title.oninput = function() {
-        var titleDisplay = document.querySelector('.title-display');
-        displayWithPlaceholder(model.title, titleDisplay, "Untitled Event");
-    };
-
-    model.type.oninput = function() {
-        var typeDisplay = document.querySelector('.card-detail-actual.what');
-        displayWithPlaceholder(model.type, typeDisplay, "Description");
-    };
-
-    model.host.oninput = function() {
-        var hostDisplay = document.querySelector('.card-detail-actual.who');
-        displayWithPlaceholder(model.host, hostDisplay, "Host");
-    };
-
-    model.starttime.oninput = function() {
-        var startTimeDisplay = document.querySelector('.card-detail-actual.starttime');
-        displayWithPlaceholder(model.starttime, startTimeDisplay, "Start Time");
-    };
-
-    model.endtime.oninput = function() {
-        var endTimeDisplay = document.querySelector('.card-detail-actual.endtime');
-        displayWithPlaceholder(model.endtime, endTimeDisplay, "End Time");
-    };
-
-
-    function validate() {
-        var self = this;
-        var allGood = false;
-        var errorMessage = "Please correct the following errors: <br>";
-        var people = getDomNodeArray('.people>div');
-
-        var validations = [{
-            errorMessage: "Please include a title.",
-            validationMethod: function() {
-                return model.title.hasChanged;
-            }
-        }, {
-            errorMessage: "Please include a description.",
-            validationMethod: function() {
-                return model.description.hasChanged;
-            }
-        }, {
-            errorMessage: "Please include guests.",
-            validationMethod: function() {
-                if (people.length > 0) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }, {
-            errorMessage: "Please include valid email addresses.",
-            validationMethod: function() {
-                var areReal = false;
-                var emailRegex = new RegExp("^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$");
-                people.forEach(function(guest, index) {
-                    var result = emailRegex.exec(guest.value);
-                    if (result && result['index'] === 0) {
-                        if (index === 0) {
-                            areReal = true;
-                        } else {
-                            areReal = areReal && true;
-                        }
-                    } else {
-                        areReal = areReal && false;
-                    }
-                });
-                return areReal;
-            }
-        }];
-
-        validations.forEach(function(val, index) {
-            if (val.validationMethod(self)) {
-                if (index === 0) {
-                    allGood = true;
-                } else {
-                    allGood = allGood && true;
-                }
-            } else {
-                allGood = allGood && false;
-                errorMessage = errorMessage + val.errorMessage + '<br>';
+                  // for callbacks
+                  self[name].oninput();
+              };
             }
         });
-        errorMessage = errorMessage.trim();
-
-        return {
-            isValid: allGood,
-            errorMessage: errorMessage
-        }
-    };
-
-    var createButton = document.querySelector('button#create');
-    createButton.onclick = function() {
-        var validState = validate();
-
-        if (!validState.isValid) {
-            var errorMessage = document.querySelector('.error-message');
-            errorMessage.innerHTML = validState.errorMessage;
-        } else {
-            alert("Valid form. Thanks for submitting!");
-        }
-    };
-
-    /*
-    Set up Google Maps autocomplete location input
-     */
-    var input = document.getElementById('location');
-    var whereDisplay = document.querySelector('.card-detail-actual.where');
-    var autocomplete = new google.maps.places.Autocomplete(input);
-
-    autocomplete.addListener('place_changed', () => {
-      let place = autocomplete.getPlace();
-      displayWithPlaceholder({
-          value: place.formatted_address
-      }, whereDisplay, "Location");
     });
+  };
+
+/*
+To create a placeholder effect. Assumes that display element starts with 'placeholder' class
+ */
+function displayWithPlaceholder(inputBinding, displayElem, placeholder) {
+    if (displayElem.classList.contains('placeholder')) {
+        displayElem.classList.remove('placeholder');
+    };
+    if (inputBinding.value === "") {
+        inputBinding.value = placeholder;
+        // let the model know that input has gone back to default
+        inputBinding.hasChanged = false;
+
+        displayElem.classList.add('placeholder');
+    }
+    displayElem.innerHTML = inputBinding.value;
+  };
+  var model = new Model();
+
+  // Update the view oninput
+  model.title.oninput = function() {
+      var titleDisplay = document.querySelector('.title-display');
+      displayWithPlaceholder(model.title, titleDisplay, "Untitled Event");
+  };
+
+  model.type.oninput = function() {
+      var typeDisplay = document.querySelector('.card-detail-actual.what');
+      displayWithPlaceholder(model.type, typeDisplay, "Description");
+  };
+
+  model.host.oninput = function() {
+      var hostDisplay = document.querySelector('.card-detail-actual.who');
+      displayWithPlaceholder(model.host, hostDisplay, "Host");
+  };
+
+  model.starttime.oninput = function() {
+      var startTimeDisplay = document.querySelector('.card-detail-actual.starttime');
+      displayWithPlaceholder(model.starttime, startTimeDisplay, "Start Time");
+  };
+
+  model.endtime.oninput = function() {
+      var endTimeDisplay = document.querySelector('.card-detail-actual.endtime');
+      displayWithPlaceholder(model.endtime, endTimeDisplay, "End Time");
+  };
+
+
+  function validate() {
+      var self = this;
+      var allGood = false;
+      var errorMessage = "Please correct the following errors: <br>";
+      var people = getDomNodeArray('.people>div');
+
+      var validations = [{
+          errorMessage: "Please include a title.",
+          validationMethod: function() {
+              return model.title.hasChanged;
+          }
+      }, {
+          errorMessage: "Please include a description.",
+          validationMethod: function() {
+              return model.description.hasChanged;
+          }
+      }, {
+          errorMessage: "Please include guests.",
+          validationMethod: function() {
+              if (people.length > 0) {
+                  return true;
+              } else {
+                  return false;
+              }
+          }
+      }, {
+          errorMessage: "Please include valid email addresses.",
+          validationMethod: function() {
+              var areReal = false;
+              var emailRegex = new RegExp("^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$");
+              people.forEach(function(guest, index) {
+                  var result = emailRegex.exec(guest.value);
+                  if (result && result['index'] === 0) {
+                      if (index === 0) {
+                          areReal = true;
+                      } else {
+                          areReal = areReal && true;
+                      }
+                  } else {
+                      areReal = areReal && false;
+                  }
+              });
+              return areReal;
+          }
+      }];
+
+      validations.forEach(function(val, index) {
+          if (val.validationMethod(self)) {
+              if (index === 0) {
+                  allGood = true;
+              } else {
+                  allGood = allGood && true;
+              }
+          } else {
+              allGood = allGood && false;
+              errorMessage = errorMessage + val.errorMessage + '<br>';
+          }
+      });
+      errorMessage = errorMessage.trim();
+
+      return {
+          isValid: allGood,
+          errorMessage: errorMessage
+      }
+  };
+
+  var createButton = document.querySelector('button#create');
+  createButton.onclick = function() {
+      var validState = validate();
+
+      if (!validState.isValid) {
+          var errorMessage = document.querySelector('.error-message');
+          errorMessage.innerHTML = validState.errorMessage;
+      } else {
+          alert("Valid form. Thanks for submitting!");
+      }
+  };
+
+  /*
+  Set up Google Maps autocomplete location input
+   */
+  var input = document.getElementById('location');
+  var whereDisplay = document.querySelector('.card-detail-actual.where');
+  var autocomplete = new google.maps.places.Autocomplete(input);
+
+  autocomplete.addListener('place_changed', () => {
+    let place = autocomplete.getPlace();
+    displayWithPlaceholder({
+        value: place.formatted_address
+    }, whereDisplay, "Location");
+  });
 })();

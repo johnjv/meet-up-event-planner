5dc51aa48a647fd4b7dbf1ab02c8e9837db0ab45
Justin Varghese John <justinjohn107@gmail.com>
2016-01-08 22:47:01 -0600

    Add validation message

--- app/scripts/login.babel.js
+++ app/scripts/login.babel.js
@@ -25,7 +25,7 @@ var firebase = new Firebase("https://meet-up-event-planner.firebaseio.com");
 let emailInput = document.querySelector('input[type="email"]');
 let passwordInput = document.querySelector('input[type="password"]');
 let submit = document.querySelector('button[type="submit"]');
-
+let validationMessage = document.querySelector('#validation-message');
 
 submit.onclick = function() {
   event.preventDefault();
@@ -34,8 +34,10 @@ submit.onclick = function() {
     password : passwordInput.value
   }, function(error, authData) {
     if (error) {
+      validationMessage.innerHTML = error;
       console.log("Login Failed!", error);
     } else {
+      validationMessage.innerHTML = 'Authenticated successfully';
       console.log("Authenticated successfully with payload:", authData);
       window.location.href = '/create-event.html';
     }

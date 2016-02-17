90ec03df0701ebb4c00b7866d657d189558f5daa
Justin Varghese John <justinjohn107@gmail.com>
2016-02-17 15:25:39 -0600

    Remove customValidity messages

--- app/scripts/create-event.js
+++ app/scripts/create-event.js
@@ -45,7 +45,6 @@
       let emailRegex = new RegExp('^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$');
       if(!emailRegex.test(newGuest.value)) {
         newGuest.classList.add('invalid');
-        newGuest.setCustomValidity('Please enter a valid email address');
         return;
       }
       newGuest.classList.remove('invalid');
@@ -81,7 +80,6 @@
     let emailRegex = new RegExp('^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$');
     if(!emailRegex.test(newGuest.value)) {
       newGuest.classList.add('invalid');
-      newGuest.setCustomValidity('Please enter a valid email address');
       return;
     }
     newGuest.classList.remove('invalid');
